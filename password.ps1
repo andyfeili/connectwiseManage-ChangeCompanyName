cb18fb024aacb5c7767bf2dc28594ac58aca6ce2
AndyFeiLi <59521017+AndyFeiLi@users.noreply.github.com>
2020-07-30 10:49:31 +0800

    Update password.ps1

--- password.ps1
+++ password.ps1
@@ -1,8 +1,8 @@
 
 	$myServer = "au.myconnectwise.net"
-	$myCompany = "XXXXXXXXXXXX"
-	$mypubkey = "XXXXXXXXXXXXXXXX"
-	$myprivatekey = "XXXXXXXXXXXXXXXX"
-	$myclientId = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
-	$username = "XXXX"
-	$password = "XXXXXXXXXXXX"
+	$myCompany = "XXXXXXXXXXXX" #company name
+	$mypubkey = "XXXXXXXXXXXXXXXX" #login to Manage > My Account > API keys
+	$myprivatekey = "XXXXXXXXXXXXXXXX" #login to Manage > My Account > API keys
+	$myclientId = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX" #register and generate clientId here https://developer.connectwise.com/ClientID
+	$username = "XXXX" #username to login to Manage
+	$password = "XXXXXXXXXXXX" #password to login to Manage
