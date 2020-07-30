f0d0624b931431e87402f2b189f80b8ff2662628
AndyFeiLi <59521017+AndyFeiLi@users.noreply.github.com>
2020-07-30 10:31:50 +0800

    Create password.ps1

---
+++ password.ps1
@@ -0,0 +1,8 @@
+
+	$myServer = "au.myconnectwise.net"
+	$myCompany = "XXXXXXXXXXXX"
+	$mypubkey = "XXXXXXXXXXXXXXXX"
+	$myprivatekey = "XXXXXXXXXXXXXXXX"
+	$myclientId = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
+	$username = "XXXX"
+	$password = "XXXXXXXXXXXX"
